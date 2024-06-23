126d789fe0899d810040bac6cf8d99939c1c3b57
ChavinduShirantha <chavindushirantha@gmail.com>
2024-06-23 15:19:44 +0530

    Created NavBar.jsx

--- src/view/common/NavBar/NavBar.jsx
+++ src/view/common/NavBar/NavBar.jsx
@@ -1,5 +1,23 @@
+import bell from '../../../images/Bell-off.png';
+import profile from '../../../images/Profile.png';
+import chevron from '../../../images/Chevron-down.png';
+
 export function NavBar() {
     return (
-        <>NavBar</>
+        <div className="w-full p-2 bg-white shadow flex justify-between h-[80px]">
+
+            <div className="flex">
+                <h1 className="ml-2 mt-4 text-black text-center text-[20px] font-semibold font-sans">Dashboard</h1>
+            </div>
+
+
+            <div className="flex flex-wrap">
+                <img className="flex pr-5 mt-4 h-[24px]" src={bell} alt=""/>
+
+                <img className="flex pr-1 mt-2 h-[40px]" src={profile} alt=""/>
+
+                <img className="flex pr-5 mt-4 h-[24px]" src={chevron} alt=""/>
+            </div>
+        </div>
     )
 }
