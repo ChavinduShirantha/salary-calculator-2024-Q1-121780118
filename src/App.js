57d7cd8e0d089b93538645eb662c75bb883933e6
ChavinduShirantha <chavindushirantha@gmail.com>
2024-06-23 09:13:09 +0530

    Created Default Layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {DefaultLayout} from "./view/common/DefaultLayout/DefaultLayout";
 
 function App() {
-  return (
-    <>
-
-    </>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route><Route path="/*" Component={DefaultLayout}></Route></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
