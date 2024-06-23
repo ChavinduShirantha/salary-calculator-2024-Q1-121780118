3a69f121b7574e9027badd8086a3d89d228eed59
ChavinduShirantha <chavindushirantha@gmail.com>
2024-06-23 10:09:10 +0530

    Created SideBar.jsx

--- src/view/common/DefaultLayout/DefaultLayout.jsx
+++ src/view/common/DefaultLayout/DefaultLayout.jsx
@@ -1,7 +1,19 @@
+import {SideBar} from "../SideBar/SideBar";
+import {NavBar} from "../NavBar/NavBar";
+import {MainContent} from "../MainContent/MainContent";
+
 export function DefaultLayout() {
     return (
-        <>
-            Default Layout
-        </>
+        <div className="mx-auto">
+            <div className="flex flex-row">
+                <div className="flex basis-1/6 bg-[#33074F]">
+                    <SideBar/>
+                </div>
+                <div className="flex basis-5/6 ">
+                    <NavBar/>
+                    <MainContent/>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/view/common/SideBar/SideBar.jsx
+++ src/view/common/SideBar/SideBar.jsx
@@ -1,5 +1,17 @@
+import dashboard_img from '../../../images/Dashboard.png';
+
 export function SideBar() {
     return (
-        <>SideBar</>
+        <div className="relative mx-auto h-screen">
+            <div className="bg-[#EBE6ED] bg-opacity-10 w-[272px] h-[80px] py-5 ">
+                <h1 className="text-white text-center text-[24px] font-semibold font-sans">Acmy Solutions</h1>
+            </div>
+            <div className="w-full h-fit mt-14 pl-4 flex flex-wrap">
+                <button className="flex bg-[#EBE6ED] bg-opacity-10 mb-4 h-[40px] w-[240px] rounded-3xl text-white
+                               text-[16px] text-left font-normal font-sans p-2"><img className="flex pr-2" src={dashboard_img}
+                                                                                 alt=""/> Dashboard
+                </button>
+            </div>
+        </div>
     )
 }
